cd716da052281e8c41e3a5de68e7a0c1d121820b
unknown <jaydeepjethwa2401@gmail.com>
2021-02-16 10:31:15 +0530

    Modified Urls in pages

--- pages/urls.py
+++ pages/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
 
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),
-    path('service/', views.service, name="service"),
-    path('faq/', views.faq, name="faq")
-    
 ]
